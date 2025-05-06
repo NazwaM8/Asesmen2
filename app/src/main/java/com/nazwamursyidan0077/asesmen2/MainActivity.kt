cf42ca660df74b9620aa46fc4114175c4cebef3d
Nazwa Mursyidan Baldan <nizuuichi@gmail.com>
2025-05-06 13:39:34 +0700

    Menambahkan Navigation Component

--- app/src/main/java/com/nazwamursyidan0077/asesmen2/MainActivity.kt
+++ app/src/main/java/com/nazwamursyidan0077/asesmen2/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.nazwamursyidan0077.asesmen2.ui.screen.MainScreen
+import com.nazwamursyidan0077.asesmen2.navigation.SetupNavGraph
 import com.nazwamursyidan0077.asesmen2.ui.theme.Asesmen2Theme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Asesmen2Theme {
-                MainScreen()
+                SetupNavGraph()
             }
         }
     }
---
+++ app/src/main/java/com/nazwamursyidan0077/asesmen2/navigation/Screen.kt
@@ -0,0 +1,5 @@
+package com.nazwamursyidan0077.asesmen2.navigation
+
+sealed class Screen(val route: String) {
+    data object Home: Screen("mainScreen")
+}
