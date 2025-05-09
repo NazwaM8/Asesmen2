21e5ad01614464d7a8c8eeb56a60ff00b8b8d50b
Nazwa Mursyidan Baldan <nizuuichi@gmail.com>
2025-05-09 14:52:45 +0700

    Membuat toggle menu list/grid

--- app/src/main/java/com/nazwamursyidan0077/asesmen2/ui/screen/MainScreen.kt
+++ app/src/main/java/com/nazwamursyidan0077/asesmen2/ui/screen/MainScreen.kt
@@ -26,9 +26,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -48,6 +52,8 @@ import com.nazwamursyidan0077.asesmen2.util.ViewModelFactory
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(navController: NavHostController) {
+    var showList by remember { mutableStateOf(true) }
+
     Scaffold (
         topBar = {
             TopAppBar(
@@ -59,6 +65,19 @@ fun MainScreen(navController: NavHostController) {
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
                 actions = {
+                    IconButton(onClick = { showList = !showList }) {
+                        Icon(
+                            painter = painterResource(
+                                if (showList) R.drawable.baseline_grid_view_24
+                                else R.drawable.baseline_view_list_24
+                            ),
+                            contentDescription = stringResource(
+                                if (showList) R.string.grid
+                                else R.string.list
+                            ),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
                     IconButton(onClick = {
                         navController.navigate(Screen.Trash.route)
                     }
